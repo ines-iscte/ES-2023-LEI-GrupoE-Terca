9b1c032bdced9a13978f406098aadf1b18756d03
ines-iscte <icaoo1@iscte-iul.pt>
2023-04-25 19:12:05 +0100

    Tests done

---
+++ src/main/java/json/tests/CsvToJsonTest.java
@@ -0,0 +1,63 @@
+package json.tests;
+
+import json.CsvToJson;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CsvToJsonTest {
+
+    private static CsvToJson test=new CsvToJson();
+    private File expectedFile = new File("C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\ES-2023-LEI-GrupoE-Terca\\arquivo.json");
+    private String path1 = "C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\ES-2023-LEI-GrupoE-Terca\\exemplo.csv";
+    private String path2 = "https://raw.githubusercontent.com/ines-iscte/ES-2023-LEI-GrupoE-Terca/main/exemplo.csv";
+
+    @Test
+    void csvToJson1() {
+        //Criação de ficheiro Json, com ficheiro local
+        String outputPath = "C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\teste1.json";
+        test.csvToJson(path1, outputPath);
+        File outputFile = new File(outputPath);
+        assertTrue(outputFile.exists());
+    }
+
+    @Test
+    void csvToJson2() throws IOException {
+        //Ficheiro Json igual a ficheiro de controlo, com ficheiro local
+        String outputPath = "C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\teste2.json";
+        test.csvToJson(path1, outputPath);
+        File outputFile = new File(outputPath);
+
+        String outputContent = new String(Files.readAllBytes(outputFile.toPath()));
+        String expectedContent = new String(Files.readAllBytes(expectedFile.toPath()));
+
+        assertEquals(outputContent, expectedContent);
+    }
+
+    @Test
+    void csvToJson3(){
+        //Criação de ficheiro Json, com ficheiro remoto
+        String outputPath = "C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\teste3.json";
+        test.csvToJson(path2, outputPath);
+        File outputFile = new File(outputPath);
+        assertTrue(outputFile.exists());
+    }
+
+    @Test
+    void csvToJson4() throws IOException {
+        //Ficheiro Json igual a ficheiro de controlo, com ficheiro remoto
+        String outputPath = "C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\teste4.json";
+        test.csvToJson(path2, outputPath);
+        File outputFile = new File(outputPath);
+
+        String outputContent = new String(Files.readAllBytes(outputFile.toPath()));
+        String expectedContent = new String(Files.readAllBytes(expectedFile.toPath()));
+
+        assertEquals(outputContent, expectedContent);
+    }
+}
