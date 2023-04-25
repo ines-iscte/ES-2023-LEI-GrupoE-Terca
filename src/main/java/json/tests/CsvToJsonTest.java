122cd7a0bf23a4ec29c8a8044cbb892f11567dcc
ines-iscte <icaoo1@iscte-iul.pt>
2023-04-25 19:21:00 +0100

    Tests done

--- src/main/java/json/tests/CsvToJsonTest.java
+++ src/main/java/json/tests/CsvToJsonTest.java
@@ -16,11 +16,12 @@ class CsvToJsonTest {
     private File expectedFile = new File("C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\ES-2023-LEI-GrupoE-Terca\\arquivo.json");
     private String path1 = "C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\ES-2023-LEI-GrupoE-Terca\\exemplo.csv";
     private String path2 = "https://raw.githubusercontent.com/ines-iscte/ES-2023-LEI-GrupoE-Terca/main/exemplo.csv";
+    private String outputPaths="C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES";
 
     @Test
     void csvToJson1() {
         //Criação de ficheiro Json, com ficheiro local
-        String outputPath = "C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\teste1.json";
+        String outputPath = outputPaths + "\\teste1.json";
         test.csvToJson(path1, outputPath);
         File outputFile = new File(outputPath);
         assertTrue(outputFile.exists());
@@ -29,7 +30,7 @@ class CsvToJsonTest {
     @Test
     void csvToJson2() throws IOException {
         //Ficheiro Json igual a ficheiro de controlo, com ficheiro local
-        String outputPath = "C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\teste2.json";
+        String outputPath = outputPaths + "\\teste2.json";
         test.csvToJson(path1, outputPath);
         File outputFile = new File(outputPath);
 
@@ -42,7 +43,7 @@ class CsvToJsonTest {
     @Test
     void csvToJson3(){
         //Criação de ficheiro Json, com ficheiro remoto
-        String outputPath = "C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\teste3.json";
+        String outputPath = outputPaths + "\\teste3.json";
         test.csvToJson(path2, outputPath);
         File outputFile = new File(outputPath);
         assertTrue(outputFile.exists());
@@ -51,7 +52,7 @@ class CsvToJsonTest {
     @Test
     void csvToJson4() throws IOException {
         //Ficheiro Json igual a ficheiro de controlo, com ficheiro remoto
-        String outputPath = "C:\\Users\\inesc\\OneDrive - ISCTE-IUL\\Documentos\\Iscte\\3º Ano\\2º Semestre\\ES\\teste4.json";
+        String outputPath = outputPaths + "\\teste4.json";
         test.csvToJson(path2, outputPath);
         File outputFile = new File(outputPath);
 
