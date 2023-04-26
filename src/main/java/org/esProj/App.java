99c50ddfe378aad36fe5a06f9251ae015bce97de
nicole-iscte <125377626+nicole-iscte@users.noreply.github.com>
2023-04-26 19:14:38 +0100

    Meti um botão antes de abrir o calendário :D

--- src/main/java/org/esProj/App.java
+++ src/main/java/org/esProj/App.java
@@ -2,10 +2,14 @@ package org.esProj;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 
 /**
@@ -17,7 +21,13 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+
+        /*FileInputStream inputstream = new FileInputStream("src/main/java/org/esProj/images/logo.png");
+        Image logo = new Image(inputstream);
+        ImageView imageView = new ImageView(logo);
+        Group root = new Group(imageView);
+        scene = new Scene(root);
+        */scene = new Scene(loadFXML("primary"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/esProj/PrimaryController.java
+++ src/main/java/org/esProj/PrimaryController.java
@@ -1,10 +1,20 @@
 package org.esProj;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URL;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javax.print.DocFlavor;
 
 public class PrimaryController {
-
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
